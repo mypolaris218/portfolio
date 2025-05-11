1334b0ad20dc774061e380bcb705e0a5c943510e
mypolaris218 <winter.polaris0214@gmail.com>
2025-05-11 12:34:32 -0500

    fix action error

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,30 @@
 import React, { useRef, useState } from 'react';
 import '../assets/styles/Contact.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
+// import Button from '@mui/material/Button';
+// import SendIcon from '@mui/icons-material/Send';
+// import TextField from '@mui/material/TextField';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
 
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
+  // const [name, setName] = useState<string>('');
+  // const [email, setEmail] = useState<string>('');
+  // const [message, setMessage] = useState<string>('');
 
-  const [nameError, setNameError] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [messageError, setMessageError] = useState<boolean>(false);
+  // const [nameError, setNameError] = useState<boolean>(false);
+  // const [emailError, setEmailError] = useState<boolean>(false);
+  // const [messageError, setMessageError] = useState<boolean>(false);
 
-  const form = useRef();
+  // const form = useRef();
 
-  const sendEmail = (e: any) => {
-    e.preventDefault();
+  // const sendEmail = (e: any) => {
+  //   e.preventDefault();
 
-    setNameError(name === '');
-    setEmailError(email === '');
-    setMessageError(message === '');
+  //   setNameError(name === '');
+  //   setEmailError(email === '');
+  //   setMessageError(message === '');
 
     /* Uncomment below if you want to enable the emailJS */
 
@@ -48,7 +48,7 @@ function Contact() {
     //   setEmail('');
     //   setMessage('');
     // }
-  };
+  // };
 
   return (
     <div id="contact">
